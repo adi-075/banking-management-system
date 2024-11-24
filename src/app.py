41b69c91158c7b70123e6c30b128aab6f05a2546
Aditya <37655818+adi-075@users.noreply.github.com>
2024-11-24 14:53:18 -0500

    Create app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_scss import Scss
+from datetime import datetime
 
 app = Flask(__name__)
+# Scss(app)
 
-
+#SQLAlchemy
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bank.db'
+# db = SQLAlchemy(app)
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return render_template("index.html")
 
+@app.route('/accounts')
+def accounts():  # put application's code here
+    return render_template("accounts.html")
 
 if __name__ == '__main__':
-    app.run()
+    with app.app_context():
+        pass
+
+    app.run(debug=True)
+
